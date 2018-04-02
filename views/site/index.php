6763ef64d3d025a32284df690f95ebfed3488b7a
test <test@test.com>
2018-04-02 16:49:31 +0700

    test env

--- views/site/index.php
+++ views/site/index.php
@@ -7,8 +7,8 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!1</h1>
-
+        <h1>Congratulations!</h1>
+        <?php echo getenv('DATABASE_URL') ?>
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
